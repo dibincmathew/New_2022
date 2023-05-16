bd88a3aaa545c3f08ae690a1ecaa66a33d2a88a6
dibin <dibincmathew@gmail.com>
2023-05-16 08:18:02 +0530

    first time commit

---
+++ src/Academy/BrowserStack.java
@@ -0,0 +1,27 @@
+package Academy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowserStack {
+
+	@Test
+	public void titlecheck() throws MalformedURLException {
+		// TODO Auto-generated method stub
+		MutableCapabilities caps=new MutableCapabilities();
+WebDriver driver=new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"),caps);
+
+driver.get("https://www.google.com/");
+Assert.assertTrue(driver.getTitle().matches("Google"));
+
+
+
+	}
+
+}
